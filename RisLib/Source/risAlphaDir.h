5464970960b97a0cfe469d13c9a8533b5f8a74ff
chrishoen <chris123hoen@gmail.com>
2018-08-29 18:02:21 -0400

    added more to RisLib

---
+++ RisLib/Source/risAlphaDir.h
@@ -0,0 +1,57 @@
+#pragma once
+
+/*==============================================================================
+This unit provides a set of functions that provide management of a
+relocatable alpha directory to be used by programs that use rislib.
+==============================================================================*/
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+namespace Ris
+{
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Constants.
+
+   // Max string size used in subsequent calls.
+   static const int cMaxAlphaStringSize = 400;
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Alpha directory management functions.
+//
+
+   // Set the alpha directory path. All subsequent calls will be relative
+   // to this.
+   void setAlphaDirectory(char* aDirPath);
+
+   // Return the alpha directory path. The input is a temp char buffer.
+   // The buffer is written to with the requested string to and a pointer
+   // to it is returned.
+   char* getAlphaDirectory();
+
+   // Return a file path to a file that lives in alpha. The first input is a
+   // temp char buffer. The second input is an file name. If the file name is
+   // null then the dir path is returned. The buffer is written to with the 
+   // requested string to and a pointer to it is returned.
+   char* getAlphaFilePath_Bin         (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_Calibration (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_Data        (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_DLL         (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_Image       (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_Lib         (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_Log         (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_RecData     (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_RollUI      (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_Settings    (char* aBuffer, char* aFileName = 0);
+   char* getAlphaFilePath_Work        (char* aBuffer, char* aFileName = 0);
+
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+}//namespace
+
---
+++ RisLib/Source/ris_priorities.h
@@ -0,0 +1,41 @@
+#ifndef _RIS_PRIORITES_H_
+#define _RIS_PRIORITES_H_
+
+/*==============================================================================
+This file provides a set of functions that are used to define default thread
+priorities for base class threads that are defined in RisLib.
+
+The priorities are exported via a set of functions instead of a set of 
+enumerations so that they will be portable. Different .cpp files are
+used for different operation systems (_priorites_win.cpp, 
+_priorites_qnx.cpp)
+
+==============================================================================*/
+
+//******************************************************************************
+namespace Ris
+{
+
+   //******************************************************************************
+   //******************************************************************************
+   //******************************************************************************
+
+   // RisLib default thread priorities
+   int get_default_thread_priority();
+   int get_default_high_thread_priority();
+   int get_default_low_thread_priority();
+
+   int get_default_qcall_thread_priority();
+   int get_default_qcall_timer_thread_priority();
+
+   int get_default_short_thread_priority();
+   int get_default_short_timer_thread_priority();
+   int get_default_long_thread_priority();
+
+   int get_default_udp_rx_thread_priority();
+   int get_default_tcp_server_thread_priority();
+   int get_default_tcp_client_thread_priority();
+
+}//namespace
+#endif
+
