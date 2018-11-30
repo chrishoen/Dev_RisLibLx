b68d3ebcc5c737377840bc2913d2114d0b28d2cc
chrishoen <chris123hoen@gmail.com>
2018-11-29 19:05:04 -0500

    reworked escape sequences, use length

--- ConsoleTest/conKeyReader.cpp
+++ ConsoleTest/conKeyReader.cpp
@@ -262,22 +262,23 @@ bool KeyReader::onKey_Escape(int aKeyIn, KeyRecord* aRecord)
 
    Prn::print(Prn::View24, "READ200*******************");
 
-   // Locals.
-   int  tKeyIn2 = 0;
-   int  tKeyIn3 = 0;
-   int  tKeyIn4 = 0;
-   int  tKeyIn5 = 0;
-   int  tKeyIn6 = 0;
-   int  tKeyIn7 = 0;
-   int  tCount = 1;
+   // Byte count. 1..7. 1 is a single escape. More than 1 is an escape
+   // sequence.
+   int  tCount = 0;
+
+   // Byte sequence. 0 is not used. 1 is 27. 2 should be 91.
+   int  tB[10];
+
+   // Flag.
    bool tFound = false;
 
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
-   // Test for not an escape sequence.
+   // Test for not an escape sequence, for a single escape.
 
-   if (getReadAvailable() == 0)
+   tCount = getReadAvailable() + 1;
+   if (tCount == 1)
    {
       // This is not an escape sequence.
       Prn::print(Prn::View24, "READ201   escape");
@@ -288,128 +289,134 @@ bool KeyReader::onKey_Escape(int aKeyIn, KeyRecord* aRecord)
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
-   // This is an escape sequence. Read the second key.
+   // This is an escape sequence. Read all of the available bytes.
 
-   Prn::print(Prn::View24, "READ201 %4d", 27);
+   Prn::print(Prn::View24, "READ200 %4d", tCount);
 
-   tKeyIn2 = readOne();
-   tCount = 2;
-   Prn::print(Prn::View24, "READ202 %4d", tKeyIn2);
-
-   // Test if the second key is not 91 .
-   if (tKeyIn2 != 91)
+   // Read the available bytes that are after the 27.
+   tB[0] = 0;  // Not used.
+   tB[1] = 27; // Escape.
+   for (int i = 2; i <= tCount; i++)
    {
-      Prn::print(Prn::View24, "READ202 IGNORE %4d", tKeyIn2);
-      // Ignore the key.
+      tB[i] = readOne();
+   }
+
+   // Guard. This is done after reading the available bytes,
+   // so that the bytes are read from the buffer.
+   if (tCount < 3 || tCount > 7)
+   {
+      printf("ESCAPE ERROR bad count %d\n", tCount);
       return false;
    }
 
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Test for sequences that do not end in 126.
-
-   // Read the third key.
-   tKeyIn3 = readOne();
-   tCount = 3;
-   Prn::print(Prn::View24, "READ203 %4d", tKeyIn3);
-
-   tFound = false;
-   if (tKeyIn3 == 65) { aRecord->mCode = cKey_UpArrow; tFound = true; }
-   if (tKeyIn3 == 66) { aRecord->mCode = cKey_DownArrow; tFound = true; }
-   if (tKeyIn3 == 67) { aRecord->mCode = cKey_RightArrow; tFound = true; }
-   if (tKeyIn3 == 68) { aRecord->mCode = cKey_LeftArrow; tFound = true; }
-
-   if (tFound) return true;
-
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Read sequences that end in 126.
-
-   // Read keys until a 126 is found.
-   tFound = false;
-   if (!tFound)
+   // Guard.
+   if (tB[2] != 91)
    {
-      tKeyIn4 = readOne();
-      tCount = 4;
-      if (tKeyIn4 == 126)  tFound = true;
-      Prn::print(Prn::View24, "READ204 %4d", tKeyIn4);
-   }
-   if (!tFound)
-   {
-      tKeyIn5 = readOne();
-      tCount = 5;
-      if (tKeyIn5 == 126)  tFound = true;
-      Prn::print(Prn::View24, "READ205 %4d", tKeyIn5);
-   }
-   if (!tFound)
-   {
-      tKeyIn6 = readOne();
-      tCount = 6;
-      if (tKeyIn6 == 126)  tFound = true;
-      Prn::print(Prn::View24, "READ206 %4d", tKeyIn6);
-   }
-   if (!tFound)
-   {
-      tKeyIn7 = readOne();
-      tCount = 7;
-      if (tKeyIn7 == 126)  tFound = true;
-      Prn::print(Prn::View24, "READ207 %4d", tKeyIn7);
-   }
-
-   if (!tFound)
-   {
-      printf("ESCAPE ERROR no 126\n");
+      printf("ESCAPE ERROR not 91 %d\n", tB[2]);
       return false;
    }
 
-   //***************************************************************************
-   //***************************************************************************
-   //***************************************************************************
-   // Test for count is 4.
+   if (tCount >= 1) Prn::print(Prn::View24, "READ201 %4d", tB[1]);
+   if (tCount >= 2) Prn::print(Prn::View24, "READ202 %4d", tB[2]);
+   if (tCount >= 3) Prn::print(Prn::View24, "READ203 %4d", tB[3]);
+   if (tCount >= 4) Prn::print(Prn::View24, "READ204 %4d", tB[4]);
+   if (tCount >= 5) Prn::print(Prn::View24, "READ205 %4d", tB[5]);
+   if (tCount >= 6) Prn::print(Prn::View24, "READ206 %4d", tB[6]);
+   if (tCount >= 7) Prn::print(Prn::View24, "READ207 %4d", tB[7]);
 
-   if (tCount == 4)
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Test for sequences of length 3.
+
+   if (tCount == 3)
    {
+      // Test the bytes.
       tFound = false;
-      if (tKeyIn3 == 49) { aRecord->mCode = cKey_Home; tFound = true; }
-      if (tKeyIn3 == 51) { aRecord->mCode = cKey_Delete; tFound = true; }
-      if (tKeyIn3 == 52) { aRecord->mCode = cKey_End; tFound = true; }
+      if (tB[3] == 65) { aRecord->mCode = cKey_UpArrow; tFound = true; }
+      if (tB[3] == 66) { aRecord->mCode = cKey_DownArrow; tFound = true; }
+      if (tB[3] == 67) { aRecord->mCode = cKey_RightArrow; tFound = true; }
+      if (tB[3] == 68) { aRecord->mCode = cKey_LeftArrow; tFound = true; }
 
+      // Guard.
       if (!tFound)
       {
-         printf("ESCAPE ERROR 401 %d\n",tKeyIn3);
+         printf("ESCAPE ERROR 302 %d\n", tB[3]);
          return false;
       }
+
+      // Done.
       return true;
    }
 
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************
-   // Test for count is 5.
+   // Test for sequences of length 4.
+
+   if (tCount == 4)
+   {
+      // Guard.
+      if (tB[4] != 126)
+      {
+         printf("ESCAPE ERROR 401 %d\n", tB[4]);
+         return false;
+      }
+
+      // Test the bytes.
+      tFound = false;
+      if (tB[3] == 49) { aRecord->mCode = cKey_Home; tFound = true; }
+      if (tB[3] == 51) { aRecord->mCode = cKey_Delete; tFound = true; }
+      if (tB[3] == 52) { aRecord->mCode = cKey_End; tFound = true; }
+
+      // Guard.
+      if (!tFound)
+      {
+         printf("ESCAPE ERROR 402 %d\n", tB[3]);
+         return false;
+      }
+
+      // Done.
+      return true;
+   }
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Test for sequences of length 5.
 
    if (tCount == 5)
    {
-      tFound = false;
-      if (tKeyIn3 == 49 && tKeyIn4 == 49) { aRecord->mCode = cKey_Function;  aRecord->mChar = 1; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 50) { aRecord->mCode = cKey_Function;  aRecord->mChar = 2; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 51) { aRecord->mCode = cKey_Function;  aRecord->mChar = 3; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 52) { aRecord->mCode = cKey_Function;  aRecord->mChar = 4; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 53) { aRecord->mCode = cKey_Function;  aRecord->mChar = 5; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 55) { aRecord->mCode = cKey_Function;  aRecord->mChar = 6; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 56) { aRecord->mCode = cKey_Function;  aRecord->mChar = 7; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 57) { aRecord->mCode = cKey_Function;  aRecord->mChar = 8; tFound = true; }
-      if (tKeyIn3 == 50 && tKeyIn4 == 48) { aRecord->mCode = cKey_Function;  aRecord->mChar = 9; tFound = true; }
-      if (tKeyIn3 == 50 && tKeyIn4 == 49) { aRecord->mCode = cKey_Function;  aRecord->mChar = 10; tFound = true; }
-      if (tKeyIn3 == 50 && tKeyIn4 == 51) { aRecord->mCode = cKey_Function;  aRecord->mChar = 11; tFound = true; }
-      if (tKeyIn3 == 50 && tKeyIn4 == 52) { aRecord->mCode = cKey_Function;  aRecord->mChar = 12; tFound = true; }
-
-      if (!tFound)
+      // Guard.
+      if (tB[5] != 126)
       {
-         printf("ESCAPE ERROR 501\n");
+         printf("ESCAPE ERROR 501 %d\n", tB[5]);
          return false;
       }
+
+      // Test the bytes.
+      tFound = false;
+      if (tB[3] == 49 && tB[4] == 49) { aRecord->mCode = cKey_Function;  aRecord->mChar = 1; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 50) { aRecord->mCode = cKey_Function;  aRecord->mChar = 2; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 51) { aRecord->mCode = cKey_Function;  aRecord->mChar = 3; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 52) { aRecord->mCode = cKey_Function;  aRecord->mChar = 4; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 53) { aRecord->mCode = cKey_Function;  aRecord->mChar = 5; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 55) { aRecord->mCode = cKey_Function;  aRecord->mChar = 6; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 56) { aRecord->mCode = cKey_Function;  aRecord->mChar = 7; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 57) { aRecord->mCode = cKey_Function;  aRecord->mChar = 8; tFound = true; }
+      if (tB[3] == 50 && tB[4] == 48) { aRecord->mCode = cKey_Function;  aRecord->mChar = 9; tFound = true; }
+      if (tB[3] == 50 && tB[4] == 49) { aRecord->mCode = cKey_Function;  aRecord->mChar = 10; tFound = true; }
+      if (tB[3] == 50 && tB[4] == 51) { aRecord->mCode = cKey_Function;  aRecord->mChar = 11; tFound = true; }
+      if (tB[3] == 50 && tB[4] == 52) { aRecord->mCode = cKey_Function;  aRecord->mChar = 12; tFound = true; }
+
+      // Guard.
+      if (!tFound)
+      {
+         printf("ESCAPE ERROR 502\n");
+         return false;
+      }
+
+      // Done.
       return true;
    }
 
@@ -420,41 +427,55 @@ bool KeyReader::onKey_Escape(int aKeyIn, KeyRecord* aRecord)
 
    if (tCount == 7)
    {
-      tFound = false;
-      if (tKeyIn3 == 49 && tKeyIn4 == 49) { aRecord->mCode = cKey_Function;  aRecord->mChar = 1; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 50) { aRecord->mCode = cKey_Function;  aRecord->mChar = 2; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 51) { aRecord->mCode = cKey_Function;  aRecord->mChar = 3; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 52) { aRecord->mCode = cKey_Function;  aRecord->mChar = 4; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 53) { aRecord->mCode = cKey_Function;  aRecord->mChar = 5; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 55) { aRecord->mCode = cKey_Function;  aRecord->mChar = 6; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 56) { aRecord->mCode = cKey_Function;  aRecord->mChar = 7; tFound = true; }
-      if (tKeyIn3 == 49 && tKeyIn4 == 57) { aRecord->mCode = cKey_Function;  aRecord->mChar = 8; tFound = true; }
-      if (tKeyIn3 == 50 && tKeyIn4 == 48) { aRecord->mCode = cKey_Function;  aRecord->mChar = 9; tFound = true; }
-      if (tKeyIn3 == 50 && tKeyIn4 == 49) { aRecord->mCode = cKey_Function;  aRecord->mChar = 10; tFound = true; }
-      if (tKeyIn3 == 50 && tKeyIn4 == 52) { aRecord->mCode = cKey_Function;  aRecord->mChar = 11; tFound = true; }
-      if (tKeyIn3 == 50 && tKeyIn4 == 53) { aRecord->mCode = cKey_Function;  aRecord->mChar = 12; tFound = true; }
-
-      if (!tFound)
+      // Guard.
+      if (tB[7] != 126)
       {
-         printf("ESCAPE ERROR 701\n");
+         printf("ESCAPE ERROR 701 %d\n", tB[7]);
          return false;
       }
 
+      // Test the bytes.
       tFound = false;
-      if (tKeyIn5 == 59 && tKeyIn6 == 50) { aRecord->mIsShift = true; tFound = true; }
-      if (tKeyIn5 == 59 && tKeyIn6 == 51) { aRecord->mIsAlt = true; tFound = true; }
-      if (tKeyIn5 == 59 && tKeyIn6 == 52) { aRecord->mIsControl = true; tFound = true; }
-      if (tKeyIn5 == 59 && tKeyIn6 == 53) { aRecord->mIsAltShift = true; tFound = true; }
-      if (tKeyIn5 == 59 && tKeyIn6 == 54) { aRecord->mIsControlShift = true; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 49) { aRecord->mCode = cKey_Function;  aRecord->mChar = 1; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 50) { aRecord->mCode = cKey_Function;  aRecord->mChar = 2; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 51) { aRecord->mCode = cKey_Function;  aRecord->mChar = 3; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 52) { aRecord->mCode = cKey_Function;  aRecord->mChar = 4; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 53) { aRecord->mCode = cKey_Function;  aRecord->mChar = 5; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 55) { aRecord->mCode = cKey_Function;  aRecord->mChar = 6; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 56) { aRecord->mCode = cKey_Function;  aRecord->mChar = 7; tFound = true; }
+      if (tB[3] == 49 && tB[4] == 57) { aRecord->mCode = cKey_Function;  aRecord->mChar = 8; tFound = true; }
+      if (tB[3] == 50 && tB[4] == 48) { aRecord->mCode = cKey_Function;  aRecord->mChar = 9; tFound = true; }
+      if (tB[3] == 50 && tB[4] == 49) { aRecord->mCode = cKey_Function;  aRecord->mChar = 10; tFound = true; }
+      if (tB[3] == 50 && tB[4] == 52) { aRecord->mCode = cKey_Function;  aRecord->mChar = 11; tFound = true; }
+      if (tB[3] == 50 && tB[4] == 53) { aRecord->mCode = cKey_Function;  aRecord->mChar = 12; tFound = true; }
 
+      // Guard.
       if (!tFound)
       {
-         printf("ESCAPE ERROR 702\n");
+         printf("ESCAPE ERROR 702 %d %d\n", tB[3], tB[4]);
          return false;
       }
+
+      // Test more bytes.
+      tFound = false;
+      if (tB[5] == 59 && tB[6] == 50) { aRecord->mIsShift = true; tFound = true; }
+      if (tB[5] == 59 && tB[6] == 51) { aRecord->mIsAlt = true; tFound = true; }
+      if (tB[5] == 59 && tB[6] == 52) { aRecord->mIsControl = true; tFound = true; }
+      if (tB[5] == 59 && tB[6] == 53) { aRecord->mIsAltShift = true; tFound = true; }
+      if (tB[5] == 59 && tB[6] == 54) { aRecord->mIsControlShift = true; tFound = true; }
+
+      // Guard.
+      if (!tFound)
+      {
+         printf("ESCAPE ERROR 703 %d %d\n", tB[5], tB[6]);
+         return false;
+      }
+
+      // Done.
       return true;
    }
 
+   // Guard.
    printf("ESCAPE ERROR 801 %d\n",tCount);
    return false;
 }
@@ -463,3 +484,61 @@ bool KeyReader::onKey_Escape(int aKeyIn, KeyRecord* aRecord)
 //******************************************************************************
 //******************************************************************************
 }//namespace
+
+
+#if 0
+escape sequence bytes.
+
+27 91 65                     up    arrow
+27 91 66                     down  arrow
+27 91 67                     right arrow
+27 91 68                     left  arrow
+
+27 91 49 126                 home
+27 91 51 126                 delete
+27 91 52 126                 end
+
+27 91 49 49       126   F1
+27 91 49 49 59 50 126   F1   shift
+27 91 49 49 59 51 126   F1   alt
+27 91 49 49 59 52 126   F1   cntl
+27 91 49 49 59 53 126   F1   alt   shift
+27 91 49 49 59 54 126   F1   cntl  shift
+
+27 91 49 49       126   F1
+27 91 49 49 59 50 126   F1   shift
+
+27 91 49 50       126   F2
+27 91 49 50 59 50 126   F2   shift
+
+27 91 49 51       126   F3
+27 91 49 51 59 50 126   F3   shift
+
+27 91 49 52       126   F4
+27 91 49 52 59 50 126   F4   shift
+
+27 91 49 53       126   F5
+27 91 49 53 59 50 126   F5   shift
+
+27 91 49 55       126   F6
+27 91 49 55 59 50 126   F6   shift
+
+27 91 49 56       126   F7
+27 91 49 56 59 50 126   F7   shift
+
+27 91 49 57       126   F8
+27 91 49 57 59 50 126   F8   shift
+
+27 91 50 48       126   F9
+27 91 50 48 59 50 126   F9   shift
+
+27 91 50 49       126   F10
+27 91 50 49 59 50 126   F10  shift
+
+27 91 50 51       126   F11
+27 91 50 52 59 50 126   F11  shift
+
+27 91 50 52       126   F12
+27 91 50 53 59 50 126   F12  shift
+
+#endif
