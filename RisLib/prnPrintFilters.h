f732db14e68229f3065ba11736118110b13e3832
chrishoen <chris123hoen@gmail.com>
2018-12-12 18:34:34 -0500

    more

--- RisLib/prnPrintFilters.h
+++ RisLib/prnPrintFilters.h
@@ -122,12 +122,12 @@ namespace Prn
    static const int  View43            = 103;
    static const int  View44            = 104;
 
-   static const int  Show1             = 105;
-   static const int  Show2             = 106;
-   static const int  Show3             = 107;
-   static const int  Show4             = 108;
-   static const int  Show5             = 109;
-   static const int  Show6             = 110;
+   static const int  Stat1             = 105;
+   static const int  Stat2             = 106;
+   static const int  Stat3             = 107;
+   static const int  Stat4             = 108;
+   static const int  Stat5             = 109;
+   static const int  Stat6             = 110;
 
    static const int  CalcInit1         = 200;
    static const int  CalcInit2         = 201;
