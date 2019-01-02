3b47f14ccb0ac0277b4b376e31fe0fe5752f9270
chrishoen <chris123hoen@gmail.com>
2019-01-02 13:35:25 -0500

    add RisLibConfig.cmake

---
+++ RisLib/RisLibConfig.cmake
@@ -0,0 +1,8 @@
+﻿
+add_library(RisLib STATIC IMPORTED)
+set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION "/var/tmp/install/lib/libRisLib.a")
+target_include_directories(RisLib INTERFACE "/var/tmp/install/include/RisLib")
+
+#set(THREADS_PREFER_PTHREAD_FLAG ON)
+#find_package(Threads REQUIRED)
+#target_link_libraries(RisLib Threads::Threads)
